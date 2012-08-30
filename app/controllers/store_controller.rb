7ed768ba7ab7293a2d79939cadf7164fb871530a
delliott90 <danny.elliott@gmail.com>
2012-08-30 17:03:52 -0300

    Big nasty upload

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,19 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @sessioncnt = sessioncount
   end
+  
+ 
+  
+  def sessioncount
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+      session[:counter] += 1
+      return session[:counter]
+  end
+    
+  
+
 end
